47e93eb37dd36b8cb5baf5c6db23ea77198cbab9
nitishwork <nitish18csu148@ncuindia.edu>
2019-01-14 12:19:36 +0530

    timeproblem

--- AddtwoNumbers.java
+++ AddtwoNumbers.java
@@ -1,9 +1,10 @@
  class AddtwoNumbers
  {
- public static void main(String[]args)
- {int num1=14,num2= 12,sum;
- sum=num1+num2;
+ 	public static void main(String[]args)
+ 	{
+ 		int num1=14,num2= 12,sum;
+ 		sum=num1+num2;
+		System.out.println("Sum of these no.s:"+sum);
 
-System.out.println("Sum of these no.s:"+sum);
-
- }}
+	}	
+}
---
+++ timeproblem.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class timeproblem
+{public static void main(String[] Strings)
+    {
+    double minutesinYear =60*24*365;
+    Scanner input  =new Scanner(System.in);
+    	System.out.print("Input the number of minutes  ");
+    	double min = input.nextDouble();
+    	long years = (long) (min / minutesinYear);
+    	int days   =(int)(min/60/24)%365;
+
+    	System.out.println((int) min +"minutes is appx."+ years+ "years and"+days+"days");
+    	}
+
+  }
